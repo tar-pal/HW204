77893745d6cfb24ac8c77c12f89f83e132e1ee27
tar-pal <tpalamarchuk@gmail.com>
2024-06-11 17:06:38 +0300

    Changed main processing

--- openweather.py
+++ openweather.py
@@ -2,8 +2,6 @@ import requests
 
 API_KEY = 'bb162c28be60f5bf4afb31a045255ad2'
 
-
-
 def get_weather_dict(city, API_KEY):
     try:
         request = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={API_KEY}&units=metric&language=ua"
@@ -17,7 +15,7 @@ def get_weather_dict(city, API_KEY):
     return data_dict
 
 def get_description(data_dict):
-    return data_dict['weather'][0]['description']
+    return data_dict['weather'][0]
 
 def get_main_data(data_dict):
     return data_dict['main']
@@ -25,7 +23,9 @@ def get_main_data(data_dict):
 def get_wind(data_dict):
     return data_dict['wind']
 
-if __name__ == 'main':
-    city = input('Enter city name:')
+if __name__ == '__main__':
+    city = input('Enter city name: ')
     data_dict = get_weather_dict(city, API_KEY)
-    print(get_description)
+    print(get_description(data_dict))
+    print(get_main_data(data_dict))
+    print(get_wind(data_dict))
