0e9e2d83e0ca6b16c6549e5443203385d0beb6f7
tar-pal <tpalamarchuk@gmail.com>
2024-06-11 17:28:31 +0300

    test_openweather.py: added parametrize for test_get_weather_dict function

--- test_openweather.py
+++ test_openweather.py
@@ -1,6 +1,12 @@
 from openweather import get_weather_dict, get_description, get_main_data, get_wind, API_KEY
 import pytest
 
-def test_get_weather_dict():
-    assert isinstance(get_weather_dict('Kyiv', API_KEY), dict), "Expected a dictionary"
+@pytest.mark.parametrize(
+    'city, api_key',
+    [
+        ('Kyiv', API_KEY)
+    ]
+)
+def test_get_weather_dict(city, api_key):
+    assert isinstance(get_weather_dict(city, api_key), dict), "Expected a dictionary"
 
